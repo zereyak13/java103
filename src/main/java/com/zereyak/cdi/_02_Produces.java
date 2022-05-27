99fcbe64ff58a5f57b05c3d93ab81afdf1e52473
Taha Zereyak <tahazryk@gmail.com>
2022-05-27 11:42:27 +0300

    Add: cdi/_02_Produces.java

---
+++ src/main/java/com/zereyak/cdi/_02_Produces.java
@@ -0,0 +1,24 @@
+package com.zereyak.cdi;
+
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.inject.Produces;
+import javax.inject.Named;
+import java.util.ArrayList;
+import java.util.List;
+
+@Named(value ="produceTuto")
+@ApplicationScoped
+public class _02_Produces {
+
+    @Produces
+    public List<String> getList(){
+        List<String> liste = new ArrayList<>();
+        liste.add("Html5");
+        liste.add("css3");
+        liste.add("js");
+        liste.add("react");
+        liste.add("angular");
+        return  liste;
+    }
+}
