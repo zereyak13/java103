984585ed638b9eae325b467539d7d5f4e72ff388
Taha Zereyak <tahazryk@gmail.com>
2022-05-27 17:06:41 +0300

    Add: No_IoC_Dı.xhtml

--- src/main/java/com/zereyak/No_IoC_DI/Employee.java
+++ src/main/java/com/zereyak/No_IoC_DI/Employee.java
@@ -1,11 +1,16 @@
 package com.zereyak.No_IoC_DI;
 
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Named;
+
+@Named(value = "cdsizCalisan")
+@ApplicationScoped
 public class Employee {
     private Boss patron;
     public Employee(){
         patron = new Boss();
     }
     public String getData(String data){
-        return patron.version("data: 17");
+        return patron.version(data);
     }
 }
