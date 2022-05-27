2f4ae8444cc7a54f26ca340e67f5f81f16f5d252
Taha Zereyak <tahazryk@gmail.com>
2022-05-27 11:58:15 +0300

    Add: _03_Inject.xhtml

--- src/main/java/com/zereyak/cdi/_03_Inject.java
+++ src/main/java/com/zereyak/cdi/_03_Inject.java
@@ -5,7 +5,7 @@ import javax.inject.Inject;
 import javax.inject.Named;
 import java.util.List;
 
-@Named(value = "InjectTuto")
+@Named(value = "injectTuto")
 @ApplicationScoped
 public class _03_Inject {
 
